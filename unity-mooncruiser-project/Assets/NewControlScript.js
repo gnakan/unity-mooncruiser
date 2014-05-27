fed5346ea8628d3efc870823c0280a8aabb01554
Gary Nakanelua <gnakan@gmail.com>
2014-05-26 21:48:05 -0700

    add car controls

---
+++ unity-mooncruiser-project/Assets/NewControlScript.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var forwardSpeed : float = 3;
+var turnSpeed : float = 2;
+
+function Update () 
+{
+	//forward speed
+	var forwardMoveAmount = Input.GetAxis("Vertical") * forwardSpeed;
+	
+	//actual turn amount
+	var turnAmount = Input.GetAxis("Horizontal") * turnSpeed;
+	
+	//rotate the vehicle
+	transform.Rotate(0,turnAmount,0);
+	
+	//push the vehicle forward with a force
+	rigidbody.AddRelativeForce(0,0,forwardMoveAmount);
+	
+}
